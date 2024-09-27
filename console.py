331619f3a530133205b065e87b6ecac31c40cf44
idarapatrick <essienidara67@gmail.com>
2024-09-27 21:53:49 +0200

    func do_create

--- console.py
+++ console.py
@@ -116,7 +116,7 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, args):
         """ Create an object of any class"""
         try:
-            class_name = args.split(" ")
+            class_name = args.split(" ")[0]  # Extract the class name
         except IndexError:
             pass
         if not class_name:
@@ -125,7 +125,8 @@ class HBNBCommand(cmd.Cmd):
         elif class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        # create Place city_id
+
+    # create Place city_id
         all_list = args.split(" ")
 
         new_instance = eval(class_name)()
@@ -133,7 +134,7 @@ class HBNBCommand(cmd.Cmd):
         for i in range(1, len(all_list)):
             key, value = tuple(all_list[i].split("="))
             if value.startswith('"'):
-                value.strip('"').replace("_", " ")
+                value = value.strip('"').replace("_", " ")
             else:
                 try:
                     value = eval(value)
